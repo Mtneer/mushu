68a601843ec2d6c1c86cc36f27c2f480509cb4f7
David Billups <davidtbillups@gmail.com>
2021-07-02 16:17:18 -0400

    server side changes should be completed. Still need to test API end points and build client-side functionality

---
+++ mushu/Models/Budget.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mushu.Models
+{
+    public class Budget
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int UserProfileId { get; set; }
+
+    }
+}
